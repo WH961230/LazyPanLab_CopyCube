ba11f01a5b5b82af8618bc97b99cbb130d58c21f
Evoreek <wh_961230@163.com>
2026-09-11 18:14:15 +0800

    计划添加实体触发控制器（问题：配置里如何区分相同的实体名称）

---
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Auto_EntityTriggerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+namespace LazyPan {
+    public class Behaviour_Auto_EntityTriggerController : Behaviour {
+        public Behaviour_Auto_EntityTriggerController(Entity entity, string behaviourSign) : base(entity, behaviourSign) {
+        }
+
+        public override void DelayedExecute() {
+            
+        }
+
+
+
+        public override void Clear() {
+            base.Clear();
+        }
+    }
+}
